c1fe85c0b7c7bde9572738d11ce08229e7fc9971
TheFirstLairron <zrwall@hotmail.com>
2015-11-24 17:10:58 -0800

    Gobble Those Gizzards My Friends

---
+++ presentationForm/presentationForm/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace presentationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonClicker = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCost6 = new System.Windows.Forms.Label();
+            this.lblCost5 = new System.Windows.Forms.Label();
+            this.lblCost4 = new System.Windows.Forms.Label();
+            this.lblCost3 = new System.Windows.Forms.Label();
+            this.lblCost2 = new System.Windows.Forms.Label();
+            this.lblCost1 = new System.Windows.Forms.Label();
+            this.lblTotal6 = new System.Windows.Forms.Label();
+            this.lblTotal5 = new System.Windows.Forms.Label();
+            this.lblTotal4 = new System.Windows.Forms.Label();
+            this.lblTotal3 = new System.Windows.Forms.Label();
+            this.lblTotal2 = new System.Windows.Forms.Label();
+            this.lblTotal1 = new System.Windows.Forms.Label();
+            this.btnPowerup6 = new System.Windows.Forms.Button();
+            this.btnPowerup5 = new System.Windows.Forms.Button();
+            this.btnPowerup4 = new System.Windows.Forms.Button();
+            this.btnPowerup3 = new System.Windows.Forms.Button();
+            this.btnPowerup2 = new System.Windows.Forms.Button();
+            this.btnPowerup1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbxTotalClicks = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClicker
+            // 
+            this.buttonClicker.BackgroundImage = global::presentationForm.Properties.Resources.Turkey;
+            this.buttonClicker.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonClicker.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonClicker.Location = new System.Drawing.Point(12, 12);
+            this.buttonClicker.Name = "buttonClicker";
+            this.buttonClicker.Size = new System.Drawing.Size(433, 404);
+            this.buttonClicker.TabIndex = 0;
+            this.buttonClicker.Text = "Eat Me";
+            this.buttonClicker.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonClicker.UseVisualStyleBackColor = true;
+            this.buttonClicker.Click += new System.EventHandler(this.buttonClicker_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCost6);
+            this.groupBox1.Controls.Add(this.lblCost5);
+            this.groupBox1.Controls.Add(this.lblCost4);
+            this.groupBox1.Controls.Add(this.lblCost3);
+            this.groupBox1.Controls.Add(this.lblCost2);
+            this.groupBox1.Controls.Add(this.lblCost1);
+            this.groupBox1.Controls.Add(this.lblTotal6);
+            this.groupBox1.Controls.Add(this.lblTotal5);
+            this.groupBox1.Controls.Add(this.lblTotal4);
+            this.groupBox1.Controls.Add(this.lblTotal3);
+            this.groupBox1.Controls.Add(this.lblTotal2);
+            this.groupBox1.Controls.Add(this.lblTotal1);
+            this.groupBox1.Controls.Add(this.btnPowerup6);
+            this.groupBox1.Controls.Add(this.btnPowerup5);
+            this.groupBox1.Controls.Add(this.btnPowerup4);
+            this.groupBox1.Controls.Add(this.btnPowerup3);
+            this.groupBox1.Controls.Add(this.btnPowerup2);
+            this.groupBox1.Controls.Add(this.btnPowerup1);
+            this.groupBox1.Location = new System.Drawing.Point(451, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 527);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Upgrades";
+            // 
+            // lblCost6
+            // 
+            this.lblCost6.AutoSize = true;
+            this.lblCost6.Location = new System.Drawing.Point(182, 472);
+            this.lblCost6.Name = "lblCost6";
+            this.lblCost6.Size = new System.Drawing.Size(71, 17);
+            this.lblCost6.TabIndex = 17;
+            this.lblCost6.Text = "Powerup6";
+            // 
+            // lblCost5
+            // 
+            this.lblCost5.AutoSize = true;
+            this.lblCost5.Location = new System.Drawing.Point(182, 387);
+            this.lblCost5.Name = "lblCost5";
+            this.lblCost5.Size = new System.Drawing.Size(71, 17);
+            this.lblCost5.TabIndex = 16;
+            this.lblCost5.Text = "Powerup5";
+            // 
+            // lblCost4
+            // 
+            this.lblCost4.AutoSize = true;
+            this.lblCost4.Location = new System.Drawing.Point(182, 304);
+            this.lblCost4.Name = "lblCost4";
+            this.lblCost4.Size = new System.Drawing.Size(71, 17);
+            this.lblCost4.TabIndex = 15;
+            this.lblCost4.Text = "Powerup4";
+            // 
+            // lblCost3
+            // 
+            this.lblCost3.AutoSize = true;
+            this.lblCost3.Location = new System.Drawing.Point(182, 216);
+            this.lblCost3.Name = "lblCost3";
+            this.lblCost3.Size = new System.Drawing.Size(71, 17);
+            this.lblCost3.TabIndex = 14;
+            this.lblCost3.Text = "Powerup3";
+            // 
+            // lblCost2
+            // 
+            this.lblCost2.AutoSize = true;
+            this.lblCost2.Location = new System.Drawing.Point(182, 131);
+            this.lblCost2.Name = "lblCost2";
+            this.lblCost2.Size = new System.Drawing.Size(71, 17);
+            this.lblCost2.TabIndex = 13;
+            this.lblCost2.Text = "Powerup2";
+            // 
+            // lblCost1
+            // 
+            this.lblCost1.AutoSize = true;
+            this.lblCost1.Location = new System.Drawing.Point(182, 46);
+            this.lblCost1.Name = "lblCost1";
+            this.lblCost1.Size = new System.Drawing.Size(71, 17);
+            this.lblCost1.TabIndex = 12;
+            this.lblCost1.Text = "Powerup1";
+            // 
+            // lblTotal6
+            // 
+            this.lblTotal6.AutoSize = true;
+            this.lblTotal6.Location = new System.Drawing.Point(329, 472);
+            this.lblTotal6.Name = "lblTotal6";
+            this.lblTotal6.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal6.TabIndex = 11;
+            this.lblTotal6.Text = "0";
+            // 
+            // lblTotal5
+            // 
+            this.lblTotal5.AutoSize = true;
+            this.lblTotal5.Location = new System.Drawing.Point(329, 387);
+            this.lblTotal5.Name = "lblTotal5";
+            this.lblTotal5.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal5.TabIndex = 10;
+            this.lblTotal5.Text = "0";
+            // 
+            // lblTotal4
+            // 
+            this.lblTotal4.AutoSize = true;
+            this.lblTotal4.Location = new System.Drawing.Point(329, 304);
+            this.lblTotal4.Name = "lblTotal4";
+            this.lblTotal4.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal4.TabIndex = 9;
+            this.lblTotal4.Text = "0";
+            // 
+            // lblTotal3
+            // 
+            this.lblTotal3.AutoSize = true;
+            this.lblTotal3.Location = new System.Drawing.Point(329, 216);
+            this.lblTotal3.Name = "lblTotal3";
+            this.lblTotal3.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal3.TabIndex = 8;
+            this.lblTotal3.Text = "0";
+            // 
+            // lblTotal2
+            // 
+            this.lblTotal2.AutoSize = true;
+            this.lblTotal2.Location = new System.Drawing.Point(329, 131);
+            this.lblTotal2.Name = "lblTotal2";
+            this.lblTotal2.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal2.TabIndex = 7;
+            this.lblTotal2.Text = "0";
+            // 
+            // lblTotal1
+            // 
+            this.lblTotal1.AutoSize = true;
+            this.lblTotal1.Location = new System.Drawing.Point(329, 46);
+            this.lblTotal1.Name = "lblTotal1";
+            this.lblTotal1.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal1.TabIndex = 6;
+            this.lblTotal1.Text = "0";
+            // 
+            // btnPowerup6
+            // 
+            this.btnPowerup6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnPowerup6.Enabled = false;
+            this.btnPowerup6.Location = new System.Drawing.Point(6, 447);
+            this.btnPowerup6.Name = "btnPowerup6";
+            this.btnPowerup6.Size = new System.Drawing.Size(170, 67);
+            this.btnPowerup6.TabIndex = 5;
+            this.btnPowerup6.UseVisualStyleBackColor = false;
+            this.btnPowerup6.Click += new System.EventHandler(this.btnPowerup6_Click);
+            // 
+            // btnPowerup5
+            // 
+            this.btnPowerup5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnPowerup5.Enabled = false;
+            this.btnPowerup5.Location = new System.Drawing.Point(6, 362);
+            this.btnPowerup5.Name = "btnPowerup5";
+            this.btnPowerup5.Size = new System.Drawing.Size(170, 67);
+            this.btnPowerup5.TabIndex = 4;
+            this.btnPowerup5.UseVisualStyleBackColor = false;
+            this.btnPowerup5.Click += new System.EventHandler(this.btnPowerup5_Click);
+            // 
+            // btnPowerup4
+            // 
+            this.btnPowerup4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnPowerup4.Enabled = false;
+            this.btnPowerup4.Location = new System.Drawing.Point(6, 279);
+            this.btnPowerup4.Name = "btnPowerup4";
+            this.btnPowerup4.Size = new System.Drawing.Size(170, 67);
+            this.btnPowerup4.TabIndex = 3;
+            this.btnPowerup4.UseVisualStyleBackColor = false;
+            this.btnPowerup4.Click += new System.EventHandler(this.btnPowerup4_Click);
+            // 
+            // btnPowerup3
+            // 
+            this.btnPowerup3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnPowerup3.Enabled = false;
+            this.btnPowerup3.Location = new System.Drawing.Point(6, 191);
+            this.btnPowerup3.Name = "btnPowerup3";
+            this.btnPowerup3.Size = new System.Drawing.Size(170, 67);
+            this.btnPowerup3.TabIndex = 2;
+            this.btnPowerup3.UseVisualStyleBackColor = false;
+            this.btnPowerup3.Click += new System.EventHandler(this.btnPowerup3_Click);
+            // 
+            // btnPowerup2
+            // 
+            this.btnPowerup2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnPowerup2.Enabled = false;
+            this.btnPowerup2.Location = new System.Drawing.Point(6, 106);
+            this.btnPowerup2.Name = "btnPowerup2";
+            this.btnPowerup2.Size = new System.Drawing.Size(170, 67);
+            this.btnPowerup2.TabIndex = 1;
+            this.btnPowerup2.UseVisualStyleBackColor = false;
+            this.btnPowerup2.Click += new System.EventHandler(this.btnPowerup2_Click);
+            // 
+            // btnPowerup1
+            // 
+            this.btnPowerup1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnPowerup1.Enabled = false;
+            this.btnPowerup1.Location = new System.Drawing.Point(6, 21);
+            this.btnPowerup1.Name = "btnPowerup1";
+            this.btnPowerup1.Size = new System.Drawing.Size(170, 67);
+            this.btnPowerup1.TabIndex = 0;
+            this.btnPowerup1.UseVisualStyleBackColor = false;
+            this.btnPowerup1.Click += new System.EventHandler(this.btnPowerup1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(190, 424);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Pounds:";
+            // 
+            // txbxTotalClicks
+            // 
+            this.txbxTotalClicks.Location = new System.Drawing.Point(34, 459);
+            this.txbxTotalClicks.Name = "txbxTotalClicks";
+            this.txbxTotalClicks.ReadOnly = true;
+            this.txbxTotalClicks.Size = new System.Drawing.Size(367, 22);
+            this.txbxTotalClicks.TabIndex = 3;
+            this.txbxTotalClicks.Text = "0";
+            this.txbxTotalClicks.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 542);
+            this.Controls.Add(this.txbxTotalClicks);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonClicker);
+            this.Name = "Form1";
+            this.Text = "Gizzard Gobblers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClicker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnPowerup6;
+        private System.Windows.Forms.Button btnPowerup5;
+        private System.Windows.Forms.Button btnPowerup4;
+        private System.Windows.Forms.Button btnPowerup3;
+        private System.Windows.Forms.Button btnPowerup2;
+        private System.Windows.Forms.Button btnPowerup1;
+        private System.Windows.Forms.Label lblTotal1;
+        private System.Windows.Forms.Label lblTotal6;
+        private System.Windows.Forms.Label lblTotal5;
+        private System.Windows.Forms.Label lblTotal4;
+        private System.Windows.Forms.Label lblTotal3;
+        private System.Windows.Forms.Label lblTotal2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbxTotalClicks;
+        private System.Windows.Forms.Label lblCost6;
+        private System.Windows.Forms.Label lblCost5;
+        private System.Windows.Forms.Label lblCost4;
+        private System.Windows.Forms.Label lblCost3;
+        private System.Windows.Forms.Label lblCost2;
+        private System.Windows.Forms.Label lblCost1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ presentationForm/presentationForm/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace presentationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Int64 Total = 0;
+        public static readonly int NUMBER_OF_POWERUPS_ALLOWED = 10;
+        
+        // ID of each Powerup
+        public const int POWERUP_1_ID = 1;
+        public const int POWERUP_2_ID = 2;
+        public const int POWERUP_3_ID = 3;
+        public const int POWERUP_4_ID = 4;
+        public const int POWERUP_5_ID = 5;
+        public const int POWERUP_6_ID = 6;
+        
+        // Number of each powerup
+        public short numberOfPowerup1 = 0;
+        public short numberOfPowerup2 = 0;
+        public short numberOfPowerup3 = 0;
+        public short numberOfPowerup4 = 0;
+        public short numberOfPowerup5 = 0;
+        public short numberOfPowerup6 = 0;
+        
+        // Names for each Powerup
+        public string nameOfPowerup1 = "Stuffing";
+        public string nameOfPowerup2 = "Cranberries";
+        public string nameOfPowerup3 = "Mashed Potatoes";
+        public string nameOfPowerup4 = "Candied Yams";
+        public string nameOfPowerup5 = "Green Bean Casserole";
+        public string nameOfPowerup6 = "Pumpkin Pie";
+        
+        // Costs of each powerup
+        public const int costOfPowerup1 = 10;
+        public const int costOfPowerup2 = 100;
+        public const int costOfPowerup3 = 1000;
+        public const int costOfPowerup4 = 10000;
+        public const int costOfPowerup5 = 100000;
+        public const int costOfPowerup6 = 1000000;
+
+        // Costs of each powerup
+        public const int scoreForPowerup1 = 1;
+        public const int scoreForPowerup2 = 10;
+        public const int scoreForPowerup3 = 100;
+        public const int scoreForPowerup4 = 1000;
+        public const int scoreForPowerup5 = 10000;
+        public const int scoreForPowerup6 = 100000;
+
+        private void buttonClicker_Click(object sender, EventArgs e)
+        {
+            addToTotal(1);
+            enableButtons();
+        }
+
+        public void addToTotal(Int64 amount)
+        {
+            this.Total += amount;
+            updateTotalDisplay();
+        }
+
+        public void subFromTotal(Int64 amount)
+        {
+            this.Total -= amount;
+            updateTotalDisplay();
+        }
+
+        public void updateTotalDisplay()
+        {
+            txbxTotalClicks.Text = Total.ToString();
+        }
+
+        public void updateUpgradeCounts()
+        {
+
+            lblTotal1.Text = numberOfPowerup1.ToString();
+            lblTotal2.Text = numberOfPowerup2.ToString();
+            lblTotal3.Text = numberOfPowerup3.ToString();
+            lblTotal4.Text = numberOfPowerup4.ToString();
+            lblTotal5.Text = numberOfPowerup5.ToString();
+            lblTotal6.Text = numberOfPowerup6.ToString();
+
+        }
+
+        public void initButtonsAndLabels()
+        {
+
+            btnPowerup1.Text = nameOfPowerup1;
+            lblCost1.Text = "Cost: " + costOfPowerup1;
+
+            btnPowerup2.Text = nameOfPowerup2;
+            lblCost2.Text = "Cost: " + costOfPowerup2;
+
+            btnPowerup3.Text = nameOfPowerup3;
+            lblCost3.Text = "Cost: " + costOfPowerup3;
+
+            btnPowerup4.Text = nameOfPowerup4;
+            lblCost4.Text = "Cost: " + costOfPowerup4;
+
+            btnPowerup5.Text = nameOfPowerup5;
+            lblCost5.Text = "Cost: " + costOfPowerup5;
+
+            btnPowerup6.Text = nameOfPowerup6;
+            lblCost6.Text = "Cost: " + costOfPowerup6;
+            
+
+        }
+
+        public void enableButtons()
+        {
+            if (Total >= costOfPowerup1 && numberOfPowerup1 < NUMBER_OF_POWERUPS_ALLOWED)
+            {
+                btnPowerup1.Enabled = true;
+            }
+            else
+            {
+                btnPowerup1.Enabled = false;
+            }
+
+            if(Total >= costOfPowerup2 && numberOfPowerup2 < NUMBER_OF_POWERUPS_ALLOWED)
+            {
+                btnPowerup2.Enabled = true;
+            }
+            else
+            {
+                btnPowerup2.Enabled = false;
+            }
+
+            if(Total >= costOfPowerup3 && numberOfPowerup3 < NUMBER_OF_POWERUPS_ALLOWED)
+            {
+                btnPowerup3.Enabled = true;
+            }
+            else
+            {
+                btnPowerup3.Enabled = false;
+            }
+
+            if (Total >= costOfPowerup4 && numberOfPowerup4 < NUMBER_OF_POWERUPS_ALLOWED)
+            {
+                btnPowerup4.Enabled = true;
+            }
+            else
+            {
+                btnPowerup4.Enabled = false;
+            }
+
+            if (Total >= costOfPowerup5 && numberOfPowerup5 < NUMBER_OF_POWERUPS_ALLOWED)
+            {
+                btnPowerup5.Enabled = true;
+            }
+            else
+            {
+                btnPowerup5.Enabled = false;
+            }
+
+            if(Total >= costOfPowerup6 && numberOfPowerup6 < NUMBER_OF_POWERUPS_ALLOWED)
+            {
+                btnPowerup6.Enabled = true;
+            }
+            else
+            {
+                btnPowerup6.Enabled = false;
+            }
+        }
+
+        public bool buyUpgrade(Int64 cost, int powerUpID)
+        {
+            if(Total >= cost)
+            {
+                switch (powerUpID)
+                {
+                    case POWERUP_1_ID:
+                        numberOfPowerup1++;
+                        break;
+                    case 2:
+                        numberOfPowerup2++;
+                        break;
+                    case 3:
+                        numberOfPowerup3++;
+                        break;
+                    case 4:
+                        numberOfPowerup4++;
+                        break;
+                    case 5:
+                        numberOfPowerup5++;
+                        break;
+                    case 6:
+                        numberOfPowerup6++;
+                        break;
+                    default:
+                        break;
+                }
+                subFromTotal(cost);
+                enableButtons();
+                return true;
+            }
+            else
+            {
+                enableButtons();
+                return false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int tempTotal = 0;
+
+            tempTotal += numberOfPowerup1 * scoreForPowerup1;
+            tempTotal += numberOfPowerup2 * scoreForPowerup2;
+            tempTotal += numberOfPowerup3 * scoreForPowerup3;
+            tempTotal += numberOfPowerup4 * scoreForPowerup4;
+            tempTotal += numberOfPowerup5 * scoreForPowerup5;
+            tempTotal += numberOfPowerup6 * scoreForPowerup6;
+
+            addToTotal(tempTotal);
+            enableButtons();
+        }
+
+        private void btnPowerup1_Click(object sender, EventArgs e)
+        {
+            buyUpgrade(costOfPowerup1, POWERUP_1_ID);
+            updateUpgradeCounts();
+        }
+
+        private void btnPowerup2_Click(object sender, EventArgs e)
+        {
+            buyUpgrade(costOfPowerup2, POWERUP_2_ID);
+            updateUpgradeCounts();
+        }
+
+        private void btnPowerup3_Click(object sender, EventArgs e)
+        {
+            buyUpgrade(costOfPowerup3, POWERUP_3_ID);
+            updateUpgradeCounts();
+        }
+
+        private void btnPowerup4_Click(object sender, EventArgs e)
+        {
+            buyUpgrade(costOfPowerup4, POWERUP_4_ID);
+            updateUpgradeCounts();
+        }
+
+        private void btnPowerup5_Click(object sender, EventArgs e)
+        {
+            buyUpgrade(costOfPowerup5, POWERUP_5_ID);
+            updateUpgradeCounts();
+        }
+
+        private void btnPowerup6_Click(object sender, EventArgs e)
+        {
+            buyUpgrade(costOfPowerup6, POWERUP_6_ID);
+            updateUpgradeCounts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initButtonsAndLabels();
+        }
+    }
+}
